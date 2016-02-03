f15e133365269bf1118900049ed200d12f7a8ff2
Hackbright Student <info@hackbrightacademy.com>
2016-02-02 17:56:59 -0800

    Set up our first route!

--- server.py
+++ server.py
@@ -2,11 +2,11 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from flask import Flask, render_template, redirect, request, flash, session
 
+from model import User, Rating, Movie, connect_to_db, db
 
 app = Flask(__name__)
 
@@ -22,7 +22,7 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
 
 
 if __name__ == "__main__":
