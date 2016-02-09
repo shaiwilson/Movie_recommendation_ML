9eee531b0866afab717a72a79988e4e6841e8566
Hackbright Student <info@hackbrightacademy.com>
2016-02-09 08:55:49 -0800

    Add prediction algorithm to user class.

--- model.py
+++ model.py
@@ -49,24 +49,26 @@ class User(db.Model):
 
         else:
             return 0.0
+            
+    def predict_rating(self, movie):
+            """Predict user's rating of a movie."""
 
-    def predict_rating(user, movie):
-        """Predict a user's rating of a movie."""
+            other_ratings = movie.ratings
+            other_users = [ r.user for r in other_ratings ]
 
-        other_ratings = movie.ratings
-        other_users = [ r.user for r in other_ratings ]
+            similarities = [
+                (self.similarity(other_user), other_user)
+                for other_user in other_users
+            ]
 
-        similarities = [
-            (user.similarity(other_user), other_user)
-            for other_user in other_users
-        ]
+            similarities.sort(reverse=True)
+            sim, best_match_user = similarities[0]
 
-        similarities.sort(reverse=True)
-        sim, best_match_user = similarities[0]
+            matched_rating = None
+            for rating in other_ratings:
+                if rating.user_id == best_match_user.user_id:
+                    return rating.score * sim
 
-        for rating in other_ratings:
-            if rating.user_id == best_match_user.user_id
-               return rating.score * sim
 
 
 class Movie(db.Model):
