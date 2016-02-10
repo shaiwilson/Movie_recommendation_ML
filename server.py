9d83a226000653a2c92c3f02147d30da9f0d234b
Hackbright Student <info@hackbrightacademy.com>
2016-02-10 09:02:14 -0800

    Add evil eye predictions to user.html.

--- server.py
+++ server.py
@@ -194,41 +194,41 @@ def show_movie(movie_id):
         # User hasn't scored, and we couldn't get a prediction
         effective_rating = None
 
-    # # Get the eye's rating, either by predicting or using real rating
+    # Get the eye's rating, either by predicting or using real rating
 
-    # the_eye = User.query.filter_by(email="the-eye@of-judgment.com").one()
-    # eye_rating = Rating.query.filter_by(
-    #     user_id=the_eye.user_id, movie_id=movie.movie_id).first()
+    the_eye = User.query.filter_by(email="the-eye@of-judgment.com").one()
+    eye_rating = Rating.query.filter_by(
+        user_id=the_eye.user_id, movie_id=movie.movie_id).first()
 
-    # if eye_rating is None:
-    #     eye_rating = the_eye.predict_rating(movie)
+    if eye_rating is None:
+        eye_rating = the_eye.predict_rating(movie)
 
-    # else:
-    #     eye_rating = eye_rating.score
+    else:
+        eye_rating = eye_rating.score
 
-    # if eye_rating and effective_rating:
-    #     difference = abs(eye_rating - effective_rating)
+    if eye_rating and effective_rating:
+        difference = abs(eye_rating - effective_rating)
 
-    # else:
-    #     # We couldn't get an eye rating, so we'll skip difference
-    #     difference = None
+    else:
+        # We couldn't get an eye rating, so we'll skip difference
+        difference = None
 
-    # # Depending on how different we are from the Eye, choose a message
+    # Depending on how different we are from the Eye, choose a message
 
-    # BERATEMENT_MESSAGES = [
-    #     "I suppose you don't have such bad taste after all.",
-    #     "I regret every decision that I've ever made that has brought me" +
-    #         " to listen to your opinion.",
-    #     "Words fail me, as your taste in movies has clearly failed you.",
-    #     "That movie is great. For a clown to watch. Idiot.",
-    #     "Words cannot express the awfulness of your taste."
-    # ]
+    BERATEMENT_MESSAGES = [
+        "I suppose you don't have such bad taste after all.",
+        "I regret every decision that I've ever made that has brought me" +
+            " to listen to your opinion.",
+        "Words fail me, as your taste in movies has clearly failed you.",
+        "That movie is great. For a clown to watch. Idiot.",
+        "Words cannot express the awfulness of your taste."
+    ]
 
-    # if difference is not None:
-    #     beratement = BERATEMENT_MESSAGES[int(difference)]
+    if difference is not None:
+        beratement = BERATEMENT_MESSAGES[int(difference)]
 
-    # else:
-    #     beratement = None
+    else:
+        beratement = None
 
     return render_template(
         "movie_details.html",
@@ -236,9 +236,9 @@ def show_movie(movie_id):
         user_rating=user_rating,
         average=avg_rating,
         prediction=prediction,
-        # eye_rating=eye_rating,
-        # difference=difference,
-        # beratement=beratement
+        eye_rating=eye_rating,
+        difference=difference,
+        beratement=beratement
         )
 
 
